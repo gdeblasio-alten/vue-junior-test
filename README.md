46739b4071f858de9407d23b41b94f1ad83292fd
gdeblasio-alten <giuseppe.deblasio@alten.it>
2022-02-18 18:04:15 +0100

    3 files modified

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 <p align="center"><a href="https://www.alten.it/" target="_blank"><img src="https://www.alten.it/wp-content/uploads/sites/5/2020/07/alten-ITALIA-120x35px.svg" width="400"></a></p>
 
-# Vue Junior Test 
+# Vue Junior Test
 
+Gantile candidato effetturae i seguenti test:
+
+1) Aprire il file main.js e completare tutte le domande di ES6
+
+2) Creare un applicazione Vue utilizzando come Fake API il sito https://jsonplaceholder.typicode.com/
+
+- Creare una barra dei menu che contenga gli articoli e che visualizzi tutti i post ( https://jsonplaceholder.typicode.com/posts )
+- Fare in modo che cliccando su un post sia possibile vedere il testo del post e i suoi commenti associati ( https://jsonplaceholder.typicode.com/comments )
+- Fare in modo che in ogni post sia possibile risondere con un nuovo commento 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
 const { createApp } = require('vue');
 import App from "./App.vue";
 
+
+// Completare il Test ES6 
+
+// Converti tutti gli elementi di un array in una stringa: esempio // Sex,Drugs,Rock,Roll
+let words = ['Sex', 'Drugs', 'Rock', 'Roll'];
+
+// Ordina gli elementi di un array
+let animals = ['cat', 'dog', 'elephant', 'bee', 'ant'];
+
+// Scrivere un oggetto person che contenga (firstname, lastname, e una funzione fullName che ritorni firstname e lastname)
+let person = {
+    firstname: 'Pippo',
+    lastname: 'Franco',
+};
+
+// Itera l'oggetto di seguito
+let dog = {
+    name: 'Bob',
+    age: 1,
+    race: 'pastore tedesco',
+};
+
+
+// Dato un'array, creare un metodo che prenda in input un valore e ritorni un array ma senza il valore dato in input
+// esempio 3 ritorna un array [1,2,4,5]
+const array = [1, 2, 3, 4, 5];
+
+
+// Scrivi un metodo che prenda in ingresso due input: la dimensione dell'array ed un valore
+// e ritorni un array composto dal valore ripetuto tante volte quante la dimensione.
+// esempio (5, 1) result = [a,a,a,a,a]
+
+
+// Esercizio reverse, dalla var testo restituire la stringa in ordine invertito.
+// Ad esempio: se la stringa di input è "Alten Italia",
+// il tuo programma dovrebbe restituire la seguente stringa "ailatI netlA".
+
+
 createApp(App).mount("#app");
